099f98de8d11eb57d33125f49b0c663bf181ffae
Davidproje <158210395+Davidproje@users.noreply.github.com>
2024-01-31 20:42:13 -0300

    Digite os itens e as quantidades.

---
+++ Tabela de itens.py
@@ -0,0 +1,15 @@
+minha_lista = []
+
+item_1_nome = input("Nome do Item 1: ")
+item_1_quantidade = int(input("Quantidade do Item 1: "))
+minha_lista.append({"nome": item_1_nome, "quantidade": item_1_quantidade})
+
+item_2_nome = input("Nome do Item 2: ")
+item_2_quantidade = int(input("Quantidade do Item 2: "))
+minha_lista.append({"nome": item_2_nome, "quantidade": item_2_quantidade})
+
+item_3_nome = input("Nome do Item 3: ")
+item_3_quantidade = int(input("Quantidade do Item 3: "))
+minha_lista.append({"nome": item_3_nome, "quantidade": item_3_quantidade})
+
+print(minha_lista)
