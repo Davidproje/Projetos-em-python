0732a837022891030c101cde6c9f8fd35f9f08c6
Davidproje <158210395+Davidproje@users.noreply.github.com>
2024-01-31 20:32:07 -0300

    Armazenara 3 itens , depois exibira quais foram digitados

---
+++ teste de lista.py
@@ -0,0 +1,12 @@
+minha_lista = []
+
+item_1 = input("Item 1: ")
+minha_lista.append(item_1)
+
+item_2 = input("Item 2: ")
+minha_lista.append(item_2)
+
+item_3 = input("Item 3: ")
+minha_lista.append(item_3)
+
+print(minha_lista)
